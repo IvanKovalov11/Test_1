1ad64651642ac938d5cb4c8d70c451401f8a1504
IvanKovalov11 <32193366+IvanKovalov11@users.noreply.github.com>
2017-10-13 12:30:40 +0300

    Update 002.cpp

--- 002.cpp
+++ 002.cpp
@@ -18,28 +18,44 @@ bool read_numbers(int array[10])
 	return success;
 }
 
+bool decr(int array[10]) 
+{
+    bool check = true;
+    for(int i = 0; i < 10; i++) 
+    {
+        if (array[i] >= array[i-1]) check=true;
+        else {check=false; break;}
+    }
+    return check;
+}
+
 int main()
 {
     int num[10];
     int b;
 	if (read_numbers(num))
 	{
-	    cin >> b;
-		for (int i = 0; i < 10; ++i)
-		{
-		    for (int j = 0; j < 10; ++j)
+	    if (decr(num))
+        {
+	        cin >> b;
+	        bool stop = true;
+		    for (int i = 0; i < 10; ++i)
 		    {
-		        if (num[i]+num[j] == b)
+		        for (int j = 0; j < 10; ++j)
 		        {
-		            cout << num[i] << ' ' << num[j] << endl;
-		        }
+		            if (num[i]+num[j] == b)
+		            {
+		                cout << num[i] << ' ' << num[j] << endl;
+		                stop = false;
+		            }
+    	        }
 		    }
-		}
-	}
-	
+		    if (stop) cout << "There is no such pair of numbers" << endl;
+        }
+        else cout << "The input sequence must be non-decreasing.";
+	}	
 	else 
 	{
-		cout << "An error has occurred while reading numbers";
+		cout<<"An error has occurred while reading the input sequence.";
 	}
-	return 0;
 }
